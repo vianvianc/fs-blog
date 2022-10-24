11d9deab0ba5d80ad5bee85d813b7c9ce8aeb785
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-10-25 00:02:31 +0200

    create-app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Med Blog</h1>
+<div id="page-body">
+  
+</div>
+    </div>
+  );
+}
+
+export default App;
